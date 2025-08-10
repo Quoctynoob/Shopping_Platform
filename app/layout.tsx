74caf16ec4c2209a15c7c531690a341628be84a0
Quoctynoob <tilburgquoc34@gmail.com>
2025-08-10 02:24:10 +0200

    fix:deploy bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
-import Sidebar from "./components/layout/sidebar";
+import Sidebar from "./components/layout/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
